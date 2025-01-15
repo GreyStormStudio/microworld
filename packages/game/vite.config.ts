268aa464e75d656d7ca6c82670ed30c72e1d7357
GreyStormStudio <2300706565@qq.com>
2025-01-15 15:38:51 +0800

    feat(game): 添加游戏组件并更新配置文件中的端口设置

--- packages/game/vite.config.ts
+++ packages/game/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import dotenv from 'dotenv'
 dotenv.config({ path: resolve(__dirname, '../config/.env') })
-const port = 1145;
+const port = process.env.GAME_PORT || 7777;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
